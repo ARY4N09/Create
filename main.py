8cb9ed5043aff3be8b86fe716cc003f48479ab91
ARY4N09 <tlaryan74@gmail.com>
2026-03-17 06:58:36 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import platform
 import os
-import subprocess
 import sys
 print('\033[0;1m[\033[1;30m•\033[0;1m]\033[0;1m LOADING')
 bit = platform.architecture()[0]
 print(f'\033[0;1m[\033[1;30m•\033[0;1m]\033[0;1m {bit} DETECTED')
 try:
     print('\033[0;1m[\033[1;30m•\033[0;1m]\033[0;1m CHECKING FOR UPDATES...')
-    subprocess.run(['git', 'pull'])
+    os.system('git pull')
     print('\033[0;1m[\033[1;32m✓\033[0;1m]\033[0;1m UPDATE CHECK COMPLETE')
 except:
     print('\033[0;1m[\033[1;33m!\033[0;1m]\033[0;1m NO GIT REPOSITORY FOUND')
